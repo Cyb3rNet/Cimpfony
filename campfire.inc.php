a1a801c852872cd89507ce597eb652a3ea293a28
Serafim Junior Dos Santos Fagundes <serafim@cyb3r.ca>
2010-03-13 22:47:31 -0500

    adjustment to config file

--- campfire.inc.php
+++ campfire.inc.php
@@ -2,27 +2,6 @@
 
 
 ################################################################################
-##	EDIT THE FOLLOWING CONSTANTS
-####
-
-
-##	THE CAMPFIRE DOMAIN AND SUBDOMAIN OF THE ACCOUNT
-##
-define('S_URL_CAMPFIRE_BASE', '');
-
-
-##	THE USER TOKEN YOU WILL USE FOR THE API REGISTERED
-##  TO THE PREVIOUS URL ACCOUT
-##
-define('S_LOGIN_CAMPFIRE_USERTOKEN', '');
-
-
-####
-##	END OF API USER EDITING
-################################################################################
-
-
-################################################################################
 ##	START INCLUDES
 ####
 
--- lib/campfire.confs.inc.php
+++ lib/campfire.confs.inc.php
@@ -1,7 +1,25 @@
 <?php
 
 
+################################################################################
+##	EDIT THE FOLLOWING CONSTANTS
+####
 
+
+##	THE CAMPFIRE DOMAIN AND SUBDOMAIN OF THE ACCOUNT
+##
+define('S_URL_CAMPFIRE_BASE', '');
+
+
+##	THE USER TOKEN YOU WILL USE FOR THE API REGISTERED
+##  TO THE PREVIOUS URL ACCOUT
+##
+define('S_LOGIN_CAMPFIRE_USERTOKEN', '');
+
+
+####
+##	END OF API USER EDITING
+################################################################################
 
 
 ?>
